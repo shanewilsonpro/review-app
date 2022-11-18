bdbfbeb6cd352ce7a86c32418e1ee4743079ee7e
Shane Wilson <shanewilsonpro@gmail.com>
2022-11-18 11:49:39 -0800

    creating new user sign up for backend

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
+require("./database");
 const userRouter = require("./routes/user");
 
 const app = express();
-app.use('/api', userRouter);
+app.use(express.json());
+app.use("/api/user", userRouter);
 
-app.get("/about", (req, res) => {
-  res.send("<h1>Hello About</h1>");
-});
+app.post(
+  "/sign-in",
+  (req, res, next) => {
+    const { email, password } = req.body;
+    
+    if (!email || !password) {
+      return res.json({ error: "email/password" });
+    }
+    next();
+  },
+  (req, res) => {
+    res.send("<h1>Hello I am on a page!</h1>");
+  }
+);
 
 app.listen(8000, () => {
   console.log("Listening port 8000");
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,16 @@
-exports.createUser = (req, res) => {
-  res.send("<h1>I will create later.</h1>");
+const User = require("../models/user");
+
+exports.create = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const oldUser = await User.findOne({ email });
+
+  if (oldUser) {
+    return res.status(401).json({ error: "This email is already registered!" });
+  }
+
+  const newUser = new User({ name, email, password });
+  await newUser.save();
+
+  res.status(201).json({ user: newUser });
 };
