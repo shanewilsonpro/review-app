b3850f3a64684c955eced6fa0bca25270b573e27
Shane Wilson <shanewilsonpro@gmail.com>
2022-12-13 14:42:31 -0800

    added role based authentication for actors

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
 const { sendError } = require("../utils/helper");
-const User = require('../models/user')
+const User = require("../models/user");
 
 exports.isAuth = async (req, res, next) => {
   const token = req.headers?.authorization;
 
+  if (!token) return sendError(res, "Invalid token!");
   const jwtToken = token.split("Bearer ")[1];
 
   if (!jwtToken) return sendError(res, "Invalid token!");
@@ -12,8 +13,16 @@ exports.isAuth = async (req, res, next) => {
   const { userId } = decode;
 
   const user = await User.findById(userId);
-  if (!user) return sendError(res, "Invalid token user not found!", 404);
+  if (!user) return sendError(res, "unauthorized access!");
 
-req.user = user;
-next();
+  req.user = user;
+  next();
+};
+
+exports.isAdmin = (req, res) => {
+  const { user } = req;
+
+  if (user.role !== "admin") return sendError(res, "unauthorized access!");
+
+  next();
 };
